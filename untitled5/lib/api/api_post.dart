2ac30dd90eb1e7ecb3719385f98db6a76310c7ec
Fedaa amin <88467954+fedaaamin@users.noreply.github.com>
2024-04-02 18:46:23 +0200

    first commit

--- untitled5/lib/api/api_post.dart
+++ untitled5/lib/api/api_post.dart
@@ -1,47 +1,6 @@
 import 'package:dio/dio.dart';
 
-class ApiPost {
-  final Dio dio = Dio();
-  final String url = "/Api/Trainees";
-  final String firstName;
-  final String gmail;
 
-  ApiPost(this.firstName, this.gmail);
-
-  postInfo() async {
-    try {
-      Response response = await dio.post(url,
-          data: {
-           "trainee": {
-             "firstName": firstName,
-             "lastName": "user2",
-             "gmail": gmail,
-             "password": "pass",
-             "gender": "Male",
-             "purpose": "Lose Weight",
-             "hasTools": true,
-             "dateOfBirth": "2002-01-31T23:47:09.002",
-             "height": 185,
-             "weight": 70,
-             "fitnessLevel": "Low",
-             "requiredCalories": 2500,
-             "availabaleDays": ["Friday", "Saturday"],
-             "traineeSports": ["Football"],
-             "traineeFoods": ["Liver"]
-           }
-          },
-          options: Options(
-            validateStatus: (_) => true,
-            contentType: Headers.jsonContentType,
-            responseType: ResponseType.json,
-          ));
-      print(response.data);
-      print('qqqqqczaefrew2wewe');
-    } on DioException catch (error) {
-      print('Error sending user data: $error');
-    }
-  }
-}
 
 class DioHelper {
   static Dio? _dio;
