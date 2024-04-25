9a1228ee2e142d8b7abf3d21a1248eccb4926ab0
Fedaa amin <88467954+fedaaamin@users.noreply.github.com>
2024-04-25 23:20:41 +0200

    update

--- untitled5/lib/sign in/sign_in_google.dart
+++ untitled5/lib/sign in/sign_in_google.dart
@@ -13,61 +13,12 @@ class SignInGoogle extends StatefulWidget {
   State<SignInGoogle> createState() => _SignInGoogleState();
 
 }
-// @override
-// void initState () {
-//   WidgetsBinding.instance.addPostFrameCallback((_){
-//     _SignInGoogleState();
-//   });
-//
-// }
+
 class _SignInGoogleState extends State<SignInGoogle> {
   String url =
       "http://11172647:60-dayfreetrial@fitnessproject-001-site1.ctempurl.com/Api/Trainees";
 
-  // Future signInWithGoogle() async {
-  //   // Trigger the authentication flow
-  //   final googleSignIn = GoogleSignIn();
-  //   googleSignIn.signOut();
-  //   final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-  //
-  //   // Obtain the auth details from the request
-  //   if (googleUser != null) {
-  //     String? name = googleUser.displayName;
-  //     String? email = googleUser.email;
-  //     final GoogleSignInAuthentication googleAuth =
-  //         await googleUser.authentication;
-  //
-  //     // Create a new credential
-  //     final credential = GoogleAuthProvider.credential(
-  //       accessToken: googleAuth.accessToken,
-  //       idToken: googleAuth.idToken,
-  //     );
-  //
-  //     // Once signed in, return the UserCredential
-  //     await FirebaseAuth.instance.signInWithCredential(credential);
-  //     print("=================================");
-  //     final dioHelper = DioHelper();
-  //     final rs = await dioHelper.getData(
-  //         url:
-  //             "http://11172647:60-dayfreetrial@fitnessproject-001-site1.ctempurl.com/Api/CheckIfTraineeExists?mail=${email}");
-  //     print("=================================");
-  //     print(rs.statusCode); // Assuming you're interested in the status code
-  //     print(rs.data);
-  //     print("==================================================");
-  //     if (rs == 1) {
-  //       Navigator.push(context,
-  //          MaterialPageRoute(builder: (context) => const NavButtons()));
-  //     } else if (rs == 0) {
-  //       final dioHelper = DioHelper();
-  //       await dioHelper
-  //           .postDate(url: url, data: {"name": name, "gmail": email});
-  //       // await ApiPost(name!, email);
-  //       Navigator.push(
-  //           context, MaterialPageRoute(builder: (context) => const Gender()));
-  //     }
-  //   }
-  // }
-  Future<void> signInWithGoogle() async {
+    Future<void> signInWithGoogle() async {
     // Trigger the authentication flow
     final googleSignIn = GoogleSignIn();
     googleSignIn.signOut();
